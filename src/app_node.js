ad7f16763d722f6c41a8c55523ae405c6c664626
Tobias von Klipstein <klipstein@uxebu.com>
2012-10-17 00:57:37 +0200

    Configure requirejs baseUrl properly

--- src/app_node.js
+++ src/app_node.js
@@ -1,5 +1,9 @@
 var requirejs = require('requirejs');
 
+requirejs.config({
+  baseUrl: __dirname
+});
+
 requirejs(['./module1', './module2'], function(mod1, mod2) {
   module.exports.mod1 = mod1;
   module.exports.mod2 = mod2;
